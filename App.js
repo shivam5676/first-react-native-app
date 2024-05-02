8ce6e01179d9d6d147ee35981f7d75ee086fcbb6
SHIVAM SINGH <ssultanpura9@GMail.com>
2024-05-02 17:50:16 +0530

    react native initialised with expo

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  // let nameFieldData=""
+  const [name, setName] = useState("");
+  const [nameArray, setNameArray] = useState([]);
+  const addNameHandler = () => {
+    setNameArray((prev) => [...prev, name]);
+  };
+  console.log(nameArray);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text>Name form</Text>
+        <TextInput
+          placeholder="enter name "
+          onChangeText={(text) => {
+            setName(text);
+          }}
+        />
+        <View style={styles.inputContainer}>
+          <Button title="add name" onPress={addNameHandler}></Button>
+        </View>
+      </View>
+      <View>
+        {nameArray.map((current,index) => {
+          return <Text key={index}>{current}</Text>;
+        })}
+      </View>
     </View>
   );
 }
@@ -13,8 +36,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    borderLeftWidth:5,
+  },
+  inputContainer: {
+    borderStyle: "solid",
+    borderColor: "black",
   },
 });
